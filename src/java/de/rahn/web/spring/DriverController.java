7ed87ba4fe2bd80afcbcd277f9acef48df9054f3
Frank W. Rahn <frank@frank-rahn.de>
2012-04-14 18:06:38 +0000

    POST und PUT

--- src/java/de/rahn/web/spring/DriverController.java
+++ src/java/de/rahn/web/spring/DriverController.java
@@ -2,11 +2,8 @@ package de.rahn.web.spring;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.util.UriTemplate;
 
 import de.rahn.services.drivers.Drivers;
 import de.rahn.services.drivers.entity.Driver;
@@ -56,12 +52,11 @@ public class DriverController {
 	 * Speichere einen Fahrer.
 	 * @param id die Id eines Fahrers
 	 * @param driver der aktuelle Fahrer
-	 * @param request der aktuelle Request
 	 * @return der geänderte Fahrer
 	 */
 	@RequestMapping(method = RequestMethod.PUT)
 	public ResponseEntity<Driver> saveDriver(@PathVariable long id,
-		@RequestBody Driver driver, HttpServletRequest request) {
+		@RequestBody Driver driver) {
 		logger
 			.info(
 				"Die Methode DriverController.saveDriver() wurde mit aufgerufen. Id={}, driver={}",
@@ -72,32 +67,19 @@ public class DriverController {
 			return new ResponseEntity<Driver>(HttpStatus.CONFLICT);
 		}
 
-		HttpHeaders headers = null;
-		HttpStatus status;
+		// Normaler Rückgabestatus
+		HttpStatus status = HttpStatus.OK;
+
 		if (driver.getId() == null) {
-			// Rückgabe: 201 Created + Location
+			// Rückgabe: 201 Created
 			status = HttpStatus.CREATED;
-			headers = new HttpHeaders();
 			driver.setId(id);
-		} else {
-			// Normaler Rückgabestatus
-			status = HttpStatus.OK;
 		}
 
 		// Speichere den Fahrer
 		driver = drivers.save(driver);
 
-		// Location erzeugen, wenn nötig
-		if (headers != null) {
-			String baseUrl =
-				String.format("%s://%s:%d%s/drivers/{id}", request.getScheme(),
-					request.getServerName(), request.getServerPort(),
-					request.getContextPath());
-			headers
-				.setLocation(new UriTemplate(baseUrl).expand(driver.getId()));
-		}
-
-		return new ResponseEntity<Driver>(driver, headers, status);
+		return new ResponseEntity<Driver>(driver, status);
 	}
 
 	/**
